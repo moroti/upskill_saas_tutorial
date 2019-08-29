e5b694925c79a132b09c84a3a25e932490286dab
Moroti Akim <moroti18@gmail.com>
2019-08-29 23:07:25 +0100

    Added custom contact form path

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
 	# Route to the about page
 	get 'about', to: 'pages#about'
 	resources :contacts
+  # This is how to creqte a custom URL
+  get 'contact-us', to: 'contacts#new'
 end
