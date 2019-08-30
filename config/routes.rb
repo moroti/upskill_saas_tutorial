a417d669561685ee9b090a5006de2499f7b1e72e
Moroti Akim <moroti18@gmail.com>
2019-08-30 15:39:45 +0100

    Cleaned up contact form routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
 	root to: 'pages#home'
 	# Route to the about page
 	get 'about', to: 'pages#about'
-	resources :contacts
+  # To generate all the possible 7 routes (actions that can be performed over a resource) in Rails
+	# resources :contacts
+  # To generate only the needed routes (here new and create contact)
+  # resources :contacts, only: [:new, :create]
+  # We generated only create routes since we have custom route for new contact
+  resources :contacts, only: :create
   # This is how to creqte a custom URL
-  get 'contact-us', to: 'contacts#new'
+  get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
