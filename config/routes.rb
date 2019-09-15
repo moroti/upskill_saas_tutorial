58f807ca048fa8c3d1509c7bb47ad174181b3b22
Moroti Akim <moroti18@gmail.com>
2019-09-15 01:51:17 +0100

    Added routes for user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 	# Route to the home page
 	root to: 'pages#home'
-
-
+  # Create the routes to users and replace the default users registration controller
   devise_for :users, controllers: {registrations: 'users/registrations'}
+  # Create the routes for the users profile
+  resources :users do
+    resource :profile
+  end
 	# Route to the about page
 	get 'about', to: 'pages#about'
   # To generate all the possible 7 routes (actions that can be performed over a resource) in Rails
